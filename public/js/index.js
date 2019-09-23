d3807044f3dd98b6b5d50ba9331912b7401e13f5
archist <webdev.archist@gmail.com>
2019-09-23 17:31:14 +0200

    git initial du 23/09

---
+++ public/js/index.js
@@ -0,0 +1,140 @@
+
+  //TABLEAU DES RESULTATS CONCERNANT LES CARACTERES (caractères, pixels, longueur, disponible, ...etc)
+  var resObjChar = {thead: {row1:[], row2:[]}, tbody: {row1: [], row2: []}}
+  var resObjStatus = [["void", "#000000"], ["✅", "#16C60C"], ["⚠️", "#EA9757"], ["🛑", "#EB6F62"]];
+  resObjChar.thead.row1 = ["&nbsp;", ["Caractères", {colspan: 3}], ["Pixels", {colspan: 3}],"&nbsp;"]
+  resObjChar.thead.row2 = ["Element", "Longueur", "Affiché", "Tronqué", "Longueur", "Disponible", "Restant", "Statut"]
+  resObjChar.tbody.row1 = ["Titre", 0, 0, 0, 0, 0, 0]
+  resObjChar.tbody.row2 = ["Description", 0, 0, 0, 0, 0, 0]
+  var $myTable = $('<table/>').append($('<thead/>')).append($('<tbody/>'))
+  var $myThead = $myTable.find(':first-child')
+  var $myTbody = $myTable.find(':last-child')
+  for(a in resObjChar.thead){
+    let $tr = $('<tr/>')
+    $myThead.append($tr)
+    for(b in resObjChar.thead[a])
+      if(typeof resObjChar.thead[a][b] === 'object')
+        $tr.append($('<th/>').attr(resObjChar.thead[a][b][1]).html(resObjChar.thead[a][b][0]))
+      else $tr.append($('<th/>').html(resObjChar.thead[a][b]))
+  }
+  for(a in resObjChar.tbody){
+    let $tr = $('<tr/>')
+    $myTbody.append($tr)
+    for(b in resObjChar.tbody[a])
+      if(typeof resObjChar.tbody[a][b] !== 'object')
+        $tr.append($('<td/>').html(resObjChar.tbody[a][b]))
+      else
+        $tr.append($('<td/>').append($('<span/>').attr({"data-status": {color:resObjStatus[0][0]}, style: {color:resObjStatus[0][1]}})))
+  }
+  $("#seo_results--score").append($myTable)
+
+  //PERMET LE CLIC SUR LES OUTILS DANS LA TOOLBOX(bas de page)
+  $(tool_box).on('click', '>div>button', function(){
+    $(myModal).addClass('on plus')
+    $(myIframe).attr({src: $(this.parentNode).attr('data-url')})
+  })
+
+
+  $(seo_results).on('click', function(e){e.stopPropagation()})
+  //PERMET DE QUITTER LE MODE MODAL
+  $(seo_results).add(myModal).on('click', function(){$(seo_results).add(myModal).removeClass('on');if(this.className=="plus")$(seo_results).add(myModal).removeClass('plus')})
+
+  //PRE-REMPLIR
+  $(inputTitle).bind(
+    {paste: function(e) {   alert(e); resTitle.innerHTML = e.target.innerHTML  },
+    {change: function(e) {    resTitle.innerHTML = e.target.innerHTML  }
+  )
+  $(inputUrl).bind(
+    {paste: function(e) {    resUrl.innerHTML = e.target.innerHTML  }},
+    {change: function(e) {    resUrl.innerHTML = e.target.innerHTML  }}
+  )
+  $(inputMetades).bind(
+    {paste: function(e) {    resMetades.innerHTML = e.target.innerHTML  }},
+    {change: function(e) {    resMetades.innerHTML = e.target.innerHTML  }}
+  )
+
+  //AFFICHER LE RESULTAT DU DIAGNOSTIQUE DANS UN MODAL
+  function startPreview() {
+    //REND LE MODAL ACTIF A LA VALIDATION
+    $(seo_results).add(myModal).addClass('on')
+
+    //RECUPERATION DES VALEUR DU FORMULAIRE
+    var seoTitle = inputTitle.value;
+    var seoUrl = inputUrl.value;
+    var seoMetades = inputMetades.value;
+
+    // Vérification de la longueur du title
+    if(seoTitle.length>0) {
+      if(seoTitle.length>71) {
+        resTitle.innerText = seoTitle.slice(0,70) + "...";
+      } else {
+        resTitle.innerText = seoTitle;
+      }
+    }
+
+    // Vérification de la longueur de la meta-description
+    if(seoMetades.length>0) {
+      if(seoMetades.length>152) {
+        resMetades.innerText = seoMetades.slice(0,152) + "...";
+      } else {
+        resMetades.innerText = seoMetades;
+      }
+    }
+
+    // Ajout de l'URL
+    if(seoUrl.length>0) {
+      resUrl.innerText = seoUrl;
+    }
+
+    if(seoMetades.length>0 || seoTitle.length>0) {
+      evaluateContent(seoTitle, seoUrl, seoMetades);
+    }
+  }
+
+  function evaluateContent(seoTitle, seoUrl, seoMetades) {
+    var alertFormat = ["⚠️", "#EA9757;"];
+    var stopFormat = ["🛑", "#EB6F62"];
+    var okFormat = ["✅", "#16C60C"];
+    var contentScore = 0;
+
+    var recommandations = "";
+
+    if(seoTitle.length<60) {
+      recommandations = recommandations + '<li class="alertFormat">' + alertFormat[0] + ' Le title est trop court (' + seoTitle.length + " caractères)</li>";
+    } else {
+      if (seoTitle.length>71) {
+        recommandations = recommandations + '<li class="stopFormat">' + stopFormat[0] + ' Le title est trop long (' + seoTitle.length + " caractères)</li>";
+      } else {
+        recommandations = recommandations + '<li class="okFormat">' + okFormat[0] + ' Le title a une longueur correcte (' + seoTitle.length + " caractères)</li>";
+        contentScore += 50;
+      }
+    }
+
+    if(seoMetades.length<140) {
+      recommandations = recommandations + '<li class="alertFormat">' + alertFormat[0] + ' La meta-description est trop courte (' + seoMetades.length + " caractères)</li>";
+    } else {
+      if (seoMetades.length>160) {
+        recommandations = recommandations + '<li class="stopFormat">' + stopFormat[0] + ' La meta-description est trop longue (' + seoMetades.length + " caractères)</li>";
+      } else {
+        recommandations = recommandations + '<li class="okFormat">' + okFormat[0] + ' La meta-description a une longueur correcte (' + seoMetades.length + " caractères)</li>";
+        contentScore += 50;
+      }
+    }
+
+    recommandationsPlaceholder.innerHTML = 	'<div id="r_container"><p>Notre avis :</p><ul> ' + recommandations + '</ul></div>';
+    scoreResult.innerText = contentScore + '/100';
+
+    if(contentScore<50) {
+      scoreResult.className="stopFormat scoreResult"
+    } else {
+      if(contentScore>80) {
+        scoreResult.className="okFormat scoreResult"
+      } else {
+        scoreResult.className="alertFormat scoreResult"
+      }
+    }
+  }
+
+  $('html, body').animate({
+    scrollTop: $("div#recommandationsPlaceholder").offset().top
+  }, 2000);
---
+++ server.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// Middleware qui enregistre les logs serveurs
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url} ${req.ip}`;
+
+	console.log(`${log}`);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append server.log')
+		}
+	});
+	next();
+});
+
+// Middleware pour la gestion des catégories
+
+app.use(function (req, res, next) {
+    switch (true) {
+        case (req.path.indexOf('/seo-tools')>(-1)):
+            res.locals.pageCategory = 'seo';
+            break;
+        case (req.path.indexOf('/sea-tools')>(-1)):
+            res.locals.pageCategory = 'sea';
+            break;
+        default:
+            res.locals.pageCategory = 'Home';
+    }
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/stylesheets'));
+app.use(express.static(__dirname + '/public/js'));
+app.use(express.static(__dirname + '/public/media'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/public/fonts'));
+
+
+// Affichage de l'année pour le copyright
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+// Affichage de la bonne catégorie pour
+hbs.registerHelper('activateMenu', function(category) {
+	var menu = "<div id='cssmenu'><ul><li name='home'><a href='/'><span>Accueil</span></a></li><li name='seo'><a href='/seo-tools'><span>Outils SEO</span></a></li><li name='sea'><a href='/sea-tools'><span>Outils SEA</span></a></li></ul></div>";
+	console.log("===> Page displayed category is " + category);
+	console.log("===> Menu is " + menu);
+
+	// var menuHtml = document.createElement('html');
+	// menuHtml.innerHTML = menu;
+	//console.log(menuHtml.getElementsByName(category)[0].value);
+
+	return menu;
+});
+
+// Redirection automatique en cas de non utilisation du /
+app.use((req, res, next) => {
+  const test = /\?[^]*\//.test(req.url);
+  if (req.url.substr(-1) === '/' && req.url.length > 1 && !test)
+    res.redirect(301, req.url.slice(0, -1));
+  else
+    next();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageH1: 'Nos meilleurs Outils pour booster votre Webmarketing',
+		welcomeMessage :'Les Outils Webmarketing de MindFruits !',
+	});
+})
+
+// Ci-après toutes les pages et sous-catégories SEO
+app.get('/seo-tools', (req, res) => {
+	res.render('seo/seo-tools.hbs', {
+		pageH1: 'Outils SEO : Boostez votre référencement',
+	});
+});
+
+app.get('/seo-tools/meta-optimizer', (req, res) => {
+	res.render('seo/meta-optimizer.hbs', {
+		pageH1: 'Outil SEO : Optimisateur de métas pour Google',
+	});
+});
+
+app.get('/seo-tools/crawler', (req, res) => {
+	res.render('seo/mf-crawler.hbs', {
+		pageH1: 'Outil SEO : Crawler de site internet',
+	});
+});
+
+// Ci-après toutes les pages et sous-catégories SEA
+app.get('/sea-tools', (req, res) => {
+	res.render('sea/sea-tools.hbs', {
+		pageH1: 'Outils SEA : Améliorez les performances de vos campagnes',
+	});
+});
+
+app.get('/sea-tools/keyword-assembler', (req, res) => {
+	res.render('sea/keyword-assembler.hbs', {
+		pageH1: 'Outil SEA : Assembleur de mots-clés pour le SEA',
+	});
+});
+
+app.get('/sea-tools/keyword-suggestor', (req, res) => {
+	res.render('sea/keyword-suggestor.hbs', {
+		pageH1: 'Outil SEA : Récupérez les suggestions des mots-clés Google',
+	});
+});
+
+app.get('/sea-tools/utm-campaign-builder', (req, res) => {
+	res.render('sea/utm-campaign-builder.hbs', {
+		pageH1: 'Outil SEA : Création de liens de taggage UTM',
+	});
+});
+
+
+app.get('/bad', (req, res) => {
+	res.send({
+		content: 'Error',
+		errorType : '404 File not found'
+	});
+})
+
+
+app.get('/anniv', (req, res) => {
+	res.render('anniv-lucas.hbs', {
+		pageH1: 'Anniversaire',
+	});
+})
+
+// Remplace n'importe quelle tentative d'accès par l'affichage de la page de maintenance
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs')
+// });
+
+// Renseignement de la mise en fonctionnement du serveur
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}.`)
+});
